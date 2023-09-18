ab238fe1a0886b72475cec4455ff899d485a50c1
dclown13 <w6092w@gmail.com>
2023-09-18 17:30:49 +0900

    09-18

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const StyledMain = styled.main`
+    padding-top: 56px;
+    display: flex;
+`;
+
+const Home = () => {
+    return (
+        <StyledMain>
+            <h1>Home</h1>
+        </StyledMain>
+    );
+};
+export default Home;
